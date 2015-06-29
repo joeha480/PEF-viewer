532a92eba7f8ea0a943af3fcb4d79e466b00fd2d
Joel Håkansson <joeha480@users.noreply.github.com>
2015-06-29 19:07:19 +0200

    Migrates PEFBrailleTableFactory to Swift

---
+++ PEFViewer/BrailleTableFactory.swift
@@ -0,0 +1,43 @@
+//
+//  BrailleTableFactory.swift
+//  PEFViewer
+//
+//  Created by Joel Håkansson on 2015-06-28.
+//  Copyright (c) 2015 Joel Håkansson. All rights reserved.
+//
+
+import Foundation
+
+@objc(PEFBrailleTableFactory)
+class BrailleTableFactory {
+	static let TABLE_INDEX_KEY = "TABLE_INDEX_KEY";
+	@objc var tables:Array<BrailleTable>;
+	@objc var selectedTable:BrailleTable?;
+	@objc var selectedTableIndex:Int{
+		didSet {
+			updateSelectedTable();
+		}
+	}
+
+	init() {
+		self.tables=[BrailleTable]();
+		tables.append(BrailleTable(name: "English (lower case)", description: "", table: " a1b'k2l`cif/msp\"e3h9o6r~djg>ntq,*5<-u8v.%{$+x!&;:4|0z7(_?w}#y)="));
+		tables.append(BrailleTable(name: "Danish", description: "", table: " a,b.k;l'cif/msp`e:h*o!r~djgæntq@å?ê-u(v\\îøë§xèç^û_ü)z\"à|ôwï#yùé"));
+		tables.append(BrailleTable(name: "German", description: "", table: " a,b.k;l\"cif|msp!e:h*o+r>djg`ntq'1?2-u(v$3960x~&<5/8)z={_4w7#y}%"));
+		tables.append(BrailleTable(name: "Swedish (CX)", description: "Matches the Swedish representation in CX", table: " a,b.k;l^cif/msp'e:h*o!r~djgäntq_å?ê-u(v@îöë§xèç\"û+ü)z=à|ôwï#yùé"));
+		self.selectedTable = nil;
+		//returns 0 if value was not stored
+		self.selectedTableIndex = NSUserDefaults.standardUserDefaults().integerForKey(BrailleTableFactory.TABLE_INDEX_KEY);
+		//didSet not called in init. See https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/Properties.html#//apple_ref/doc/uid/TP40014097-CH14-ID254
+		updateSelectedTable();
+	}
+	
+	private func updateSelectedTable() {
+		let si = self.selectedTableIndex % self.tables.count;
+		selectedTable = tables[si];
+		let defaults = NSUserDefaults.standardUserDefaults();
+		defaults.setInteger(si, forKey: BrailleTableFactory.TABLE_INDEX_KEY);
+		defaults.synchronize();
+	}
+
+}
